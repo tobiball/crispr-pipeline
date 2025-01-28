55cc4c6c43c61fb98f493bf5c74750c8c0647f94
Tobias Ballauff <tobias@ballauff.net>
2025-01-28 11:45:14 +0100

    Filter for viability

--- validator/src/data_handling/genome_crispr.rs
+++ validator/src/data_handling/genome_crispr.rs
@@ -1,5 +1,5 @@
 use polars::prelude::*;
-use tracing::{error, info};
+use tracing::{debug, error, info};
 use crate::helper_functions::read_csv;
 use anyhow::Error;
 
@@ -10,6 +10,23 @@ pub struct GenomeCrisprDatsets {
 
 use polars::prelude::*;
 use crate::models::Dataset;
+use polars::prelude::*;
+use polars::lazy::dsl::*;
+
+fn create_sub_dataframe(df_original: DataFrame) -> PolarsResult<DataFrame> {
+    let df_sub = df_original
+        .lazy()
+        .filter(
+            col("condition").eq(lit("viability"))
+                .or(col("condition").eq(lit("viability after 36 days")))
+                .or(col("condition").eq(lit("viability after 21 days")))
+                .or(col("condition").eq(lit("viability after 25 days")))
+        )
+        .collect()?;
+
+    Ok(df_sub)
+}
+
 
 impl Dataset for GenomeCrisprDatsets {
     fn load(&self) -> PolarsResult<DataFrame> {
@@ -23,36 +40,40 @@ impl Dataset for GenomeCrisprDatsets {
                 return Err(e);
             }
         };
-    println!("{}", &df_original);
+        debug!("{}", &df_original);
         // Work with `df_original`
         let total_guides = df_original.height();
         info!("Number of sgRNAs to check: {}", total_guides);
 
-        // Get the "condition" column
-        let name_series = df_original.column("condition")?;
-        let unique_names = name_series.unique()?;
-        let unique_name_vec: Vec<String> = unique_names
-            .str()?
-            .into_no_null_iter()
-            .map(|s| s.to_string())
-            .collect();
-        println!("Unique name vector: {:?}", unique_name_vec);
 
-        let df_result = df_original
+
+        let df_filtered = create_sub_dataframe(df_original)?;
+
+        debug!("{:?}", df_filtered);
+
+        // let name_series = df_filtered.column("condition")?;
+        // let unique_names = name_series.unique()?;
+        // let unique_name_vec: Vec<String> = unique_names
+        //     .str()?
+        //     .into_no_null_iter()
+        //     .map(|s| s.to_string())
+        //     .collect();
+        // debug!("Unique name vector: {:?}", unique_name_vec);
+
+        let df_result = df_filtered
             .clone()
             .lazy()
             .group_by([col("pubmed")])
             .agg([col("condition").unique().alias("unique_conditions")])
             .collect()?;
 
-        println!("{:?}", df_result);
-
         for i in df_result["unique_conditions"].phys_iter() {
-            println!("{:?}", i);
+            debug!("{:?}", i);
         }
 
         // Return the original DataFrame, or lazy_result, or whichever you need
-        Ok(df_original)
+        Ok(df_filtered)
     }
 }
+
 
